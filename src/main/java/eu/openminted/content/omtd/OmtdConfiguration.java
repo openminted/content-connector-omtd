3a08f3a40a3e3455b9b99b64ccaaa9cb067c8c85
John Diplas <Jdiplas@gmail.com>
2018-03-15 12:27:02 +0200

    Bug fixes for facet display

--- src/main/java/eu/openminted/content/omtd/OmtdConfiguration.java
+++ src/main/java/eu/openminted/content/omtd/OmtdConfiguration.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
-@ComponentScan("eu.openminted.content.omtd")
+@ComponentScan({"eu.openminted.content.omtd", "eu.openminted.content.connector.utils.converters"})
 public class OmtdConfiguration {
 
 }
--- src/main/java/eu/openminted/content/omtd/OmtdContentConnector.java
+++ src/main/java/eu/openminted/content/omtd/OmtdContentConnector.java
@@ -3,14 +3,25 @@ package eu.openminted.content.omtd;
 import eu.openminted.content.connector.ContentConnector;
 import eu.openminted.content.connector.Query;
 import eu.openminted.content.connector.SearchResult;
+import eu.openminted.content.connector.utils.SearchExtensions;
+import eu.openminted.content.connector.utils.converters.LanguageTypeConverter;
+import eu.openminted.content.connector.utils.converters.PublicationTypeConverter;
+import eu.openminted.content.connector.utils.converters.RightsStmtNameConverter;
 import eu.openminted.content.connector.utils.faceting.OMTDFacetEnum;
 import eu.openminted.content.connector.utils.faceting.OMTDFacetLabels;
+import eu.openminted.content.connector.utils.language.LanguageUtils;
+import eu.openminted.registry.core.domain.Facet;
+import eu.openminted.registry.core.domain.Value;
+import eu.openminted.registry.domain.DocumentTypeEnum;
+import eu.openminted.registry.domain.PublicationTypeEnum;
+import eu.openminted.registry.domain.RightsStatementEnum;
 import org.apache.log4j.Logger;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.SolrQuery;
 import org.apache.solr.client.solrj.SolrServerException;
 import org.apache.solr.client.solrj.StreamingResponseCallback;
 import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.apache.solr.client.solrj.response.FacetField;
 import org.apache.solr.client.solrj.response.QueryResponse;
 import org.apache.solr.common.SolrDocument;
 import org.apache.solr.common.params.CursorMarkParams;
@@ -60,6 +71,15 @@ public class OmtdContentConnector implements ContentConnector{
     @Autowired
     private OMTDFacetLabels omtdFacetLabels;
 
+    @Autowired
+    private RightsStmtNameConverter rightsStmtNameConverter;
+
+    @Autowired
+    private PublicationTypeConverter publicationTypeConverter;
+
+    @Autowired
+    private LanguageTypeConverter languageTypeConverter;
+
     /**
      * Search method for browsing metadata
      *
@@ -73,8 +93,13 @@ public class OmtdContentConnector implements ContentConnector{
             tmpQuery.setKeyword("*:*");
         }
 
+        buildSort(tmpQuery);
+        buildFields(tmpQuery);
+        buildParams(tmpQuery);
+
         tmpQuery.getParams().remove(OMTDFacetEnum.SOURCE.value());
         tmpQuery.getFacets().remove(OMTDFacetEnum.SOURCE.value());
+
 
         SearchResult searchResult = new SearchResult();
         try {
@@ -87,21 +112,61 @@ public class OmtdContentConnector implements ContentConnector{
                 searchResult.setTo((int) response.getResults().getStart() + response.getResults().size());
                 searchResult.setTotalHits((int) response.getResults().getNumFound());
 
-                for (SolrDocument document : response.getResults()) {
-                    String xml = document.getFieldValue(queryOutputField).toString().replaceAll("[\\[\\]]", "");
-                    searchResult.getPublications().add(document.getFieldValue("id").toString());
-                }
+//                for (SolrDocument document : response.getResults()) {
+//                    String xml = document.getFieldValue(queryOutputField).toString().replaceAll("[\\[\\]]", "");
+//                    xml = xml.trim();
+//                    parser.parse(new InputSource(new StringReader(xml)));
+//                    searchResult.getPublications().add(parser.getOMTDPublication());
+//                }
             } else {
                 searchResult.setFrom(query.getFrom());
                 searchResult.setTo(query.getFrom());
                 searchResult.setTotalHits(0);
             }
 
+            Map<String, Facet> facets = new HashMap<>();
+
+            if (response.getFacetFields() != null) {
+                for (FacetField facetField : response.getFacetFields()) {
+                    Facet facet = buildFacet(facetField);
+                    if (facet != null && facet.getValues() != null && facet.getValues().size() > 0) {
+                        // merge values that contain the same value.value
+                        facet.setValues(SearchExtensions.mergeValues(facet));
+                        if (!facets.containsKey(facet.getField())) {
+                            facets.put(facet.getField(), facet);
+                        } else {
+                            // in case another facet with the same facet.field exists,
+                            // merge the two facets and add the product to the map
+                            // instead of the initial facet
+                            facets.put(facet.getField(), SearchExtensions.mergeFacet(facets.get(facet.getField()), facet));
+                        }
+                    }
+                }
+
+                Facet facet = facets.get(OMTDFacetEnum.DOCUMENT_TYPE.value());
+                if (facet != null) {
+                    String abstractLabel = omtdFacetLabels.getDocumentTypeLabelFromEnum(DocumentTypeEnum.WITH_ABSTRACT_ONLY);
+                    Value abstractsValue = new Value();
+                    abstractsValue.setValue(abstractLabel);
+                    abstractsValue.setLabel(abstractLabel);
+                    int count = 0;
+                    for (Value value : facet.getValues()) {
+                        count += value.getCount();
+                    }
+                    abstractsValue.setCount(searchResult.getTotalHits() - count);
+                    facet.getValues().add(abstractsValue);
+                }
+            }
+
+            searchResult.setFacets(new ArrayList<>(facets.values()));
 
         } catch (SolrServerException e) {
           log.error("Error in content connector OMTD ",e);
         }
-
+        ArrayList<String> tempArrayList = new ArrayList<>();
+        tempArrayList.add("OMTD");
+        tmpQuery.getParams().put("source", tempArrayList);
+        tmpQuery.getFacets().add(OMTDFacetEnum.SOURCE.value());
         return searchResult;
     }
 
@@ -329,6 +394,135 @@ public class OmtdContentConnector implements ContentConnector{
         } catch (SolrServerException e) {
             log.info("Fetching metadata has been interrupted. See Debug for more information!");
             log.debug("OpenAireSolrClient.fetchMetadata", e);
+        }
+    }
+
+
+    private void buildSort(Query query) {
+
+        if (query.getParams() == null) query.setParams(new HashMap<>());
+        if (!query.getParams().containsKey("sort")) {
+            query.getParams().put("sort", new ArrayList<>());
+            query.getParams().get("sort").add("id desc");
+        } else {
+            if (!query.getParams().get("sort").contains("id desc")
+                    && !query.getParams().get("sort").contains("id asc")) {
+                query.getParams().get("sort").add("id desc");
+            }
+        }
+    }
+
+    private void buildFields(Query query) {
+
+        if (query.getParams() == null) query.setParams(new HashMap<>());
+
+        if (!query.getParams().containsKey("fl")) {
+            query.getParams().put("fl", new ArrayList<>());
+            query.getParams().get("fl").add(queryOutputField);
+        } else {
+            if (!query.getParams().get("fl").contains(queryOutputField)) {
+                query.getParams().get("fl").add(queryOutputField);
+            }
+        }
+    }
+
+    private Facet buildFacet(FacetField facetField) {
+
+        Facet facet = null;
+        String facetFieldName = facetField.getName().toLowerCase();
+        facet = new Facet();
+        String field =facetFieldName;
+        facet.setField(field);
+        OMTDFacetEnum facetEnum = OMTDFacetEnum.fromValue(field);
+        facet.setLabel(omtdFacetLabels.getFacetLabelsFromEnum(facetEnum));
+        List<Value> values = new ArrayList<>();
+        LanguageUtils languageUtils = new LanguageUtils();
+
+        for (FacetField.Count count : facetField.getValues()) {
+            Value value = new Value();
+            if (field.equalsIgnoreCase(OMTDFacetEnum.RIGHTS.value())) {
+                RightsStatementEnum rightsStatementEnum = rightsStmtNameConverter.convertToOMTD(count.getName());
+
+                if (rightsStatementEnum != null
+                        && !omtdFacetLabels.getRightsStmtLabelFromEnum(rightsStatementEnum).isEmpty()) {
+                    value.setValue(omtdFacetLabels.getRightsStmtLabelFromEnum(rightsStatementEnum));
+                    value.setLabel(omtdFacetLabels.getRightsStmtLabelFromEnum(rightsStatementEnum));
+                } else {
+                    value.setValue(count.getName());
+                    value.setLabel(count.getName());
+                }
+            } else if (field.equalsIgnoreCase(OMTDFacetEnum.PUBLICATION_TYPE.value())) {
+                PublicationTypeEnum publicationTypeEnum = publicationTypeConverter.convertToOMTD(count.getName());
+
+                if (!omtdFacetLabels.getPublicationTypeLabelFromEnum(publicationTypeEnum).isEmpty()) {
+                    value.setValue(omtdFacetLabels.getPublicationTypeLabelFromEnum(publicationTypeEnum));
+                    value.setLabel(omtdFacetLabels.getPublicationTypeLabelFromEnum(publicationTypeEnum));
+                } else {
+                    value.setValue(count.getName());
+                    value.setLabel(count.getName());
+                }
+            } else if (field.equalsIgnoreCase(OMTDFacetEnum.DOCUMENT_LANG.value())) {
+                String language = languageTypeConverter.convertCodeToLanguage(count.getName());
+
+                if (language != null) {
+                    value.setValue(language.toLowerCase());
+                    value.setLabel(languageUtils.getLangCodeToName().get(value.getValue()));
+                }
+            } else if (field.equalsIgnoreCase(OMTDFacetEnum.PUBLICATION_YEAR.value())) {
+                value.setValue(count.getName().substring(0, 4));
+                value.setLabel(count.getName().substring(0, 4));
+
+            } else if (field.equalsIgnoreCase(OMTDFacetEnum.DOCUMENT_TYPE.value())) {
+                String fulltextDocumentType = omtdFacetLabels
+                        .getDocumentTypeLabelFromEnum(DocumentTypeEnum.WITH_FULL_TEXT);
+
+
+                for (Value value1 : values) {
+                    if (value1.getValue().equalsIgnoreCase(fulltextDocumentType)) {
+                        value = value1;
+                        break;
+                    }
+                }
+                value.setValue(fulltextDocumentType);
+                value.setLabel(fulltextDocumentType);
+
+            } else {
+                value.setValue(count.getName());
+                value.setLabel(count.getName());
+            }
+
+            value.setCount(value.getCount() + (int)count.getCount());
+
+            if (!values.contains(value))
+                values.add(value);
+        }
+        facet.setValues(values);
+
+        return facet;
+    }
+
+    private void buildParams(Query query) {
+
+        Map<String, List<String>> omtdParams = new HashMap<>();
+
+        if (query.getParams() != null && query.getParams().size() > 0) {
+
+            for (String key : query.getParams().keySet()) {
+
+                if (key.equalsIgnoreCase(OMTDFacetEnum.SOURCE.value())) continue;
+
+                else if (key.equalsIgnoreCase(OMTDFacetEnum.DOCUMENT_LANG.value())) {
+                    String languageKey = key.toLowerCase();
+                    omtdParams.put(languageKey, new ArrayList<>());
+
+                    // populate params with language code Ids
+                    for (String languageValue : query.getParams().get(key)) {
+                        omtdParams.get(languageKey).add(languageValue.toLowerCase());
+                    }
+                }
+
+                query.setParams(omtdParams);
+            }
         }
     }
 
